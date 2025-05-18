a6318da55443218ea33cbcf5ed7b52ba46133583
Matty <matyshemi@gmail.com>
2025-05-18 14:13:06 -0400

    Netflix complitted

--- netflix-clone/src/Components/Header/Header.jsx
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -1,20 +1,5 @@
-// import React from "react";
-// import "./header.css";
-
-// const Header = () => {
-//   return (
-//     <div className="heder_outer_container">
-//       <div className="header_container">
-//         <div className="header_left"></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
 import React from "react";
-import logo from "../../Assets/images/netflix.png";
+import logo from "../../assets/images/netflix.png";
 import "./Header.css";
 import { IoSearchOutline } from "react-icons/io5";
 import { IoMdNotificationsOutline } from "react-icons/io";
